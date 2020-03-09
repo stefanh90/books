9dd3be1df6d3bf171fcabde714fabb734d78deae
Stefan <stefan@elevatedigital.nl>
2020-03-09 10:21:48 +0100

    Changed name

--- tests/Unit/BookTest.php
+++ tests/Unit/BookTest.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ArticleTest extends TestCase
+class BookTest extends TestCase
 {
     use RefreshDatabase;
 
